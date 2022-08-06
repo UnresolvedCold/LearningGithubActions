d1f64af1936dd5ac702b0a7b682adb0705e5eb09
Shubham Kumar <unresolved.shubham@gmail.com>
2022-08-07 02:13:25 +0530

    Init

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void demoTest() {
+		Assert.isTrue(true, "Good");
+	}
+
+}
